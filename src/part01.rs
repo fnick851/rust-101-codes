8e4cbeaa3f097db0eb154588ec49ae1e7dffc85d
Noah <hanlun.song@gmail.com>
2021-05-21 20:21:29 -0400

    expression & inherent implementation

--- src/part01.rs
+++ src/part01.rs
@@ -4,19 +4,26 @@
 // For Rust to compile this file, make sure to enable the corresponding line
 // in `main.rs` before going on.
 
-
 // ## Expression-based programming
-fn sqr(i: i32) -> i32 { i * i }
+fn sqr(i: i32) -> i32 {
+    i * i
+}
 
 // Conditionals are also just expressions. This is comparable to the ternary `? :` operator
 // from languages like C.
-fn abs(i: i32) -> i32 { if i >= 0 { i } else { -i } }
+fn abs(i: i32) -> i32 {
+    if i >= 0 {
+        i
+    } else {
+        -i
+    }
+}
 
 enum NumberOrNothing {
     Number(i32),
-    Nothing
+    Nothing,
 }
-use self::NumberOrNothing::{Number,Nothing};
+use self::NumberOrNothing::{Nothing, Number};
 fn number_or_default(n: NumberOrNothing, default: i32) -> i32 {
     match n {
         Nothing => default,
@@ -26,19 +33,29 @@ fn number_or_default(n: NumberOrNothing, default: i32) -> i32 {
 
 // It is even the case that blocks are expressions, evaluating to the last expression they contain.
 fn compute_stuff(x: i32) -> i32 {
-    let y = { let z = x*x; z + 14 };
-    y*y
+    let y = {
+        let z = x * x;
+        z + 14
+    };
+    y * y
 }
 
 // Let us now refactor `vec_min`.
 fn vec_min(v: Vec<i32>) -> NumberOrNothing {
     fn min_i32(a: i32, b: i32) -> i32 {
-        unimplemented!()
+        if a > b {
+            b
+        } else {
+            a
+        }
     }
 
     let mut min = Nothing;
     for e in v {
-        unimplemented!()
+        min = Number(match min {
+            Nothing => (e),
+            Number(n) => (min_i32(n, e)),
+        })
     }
     min
 }
@@ -56,19 +73,32 @@ impl NumberOrNothing {
     }
 }
 
+// **Exercise 01.1**: Write a function `vec_sum` that computes the sum of all values of a `Vec<i32>`.
+fn vec_sum(v: Vec<i32>) -> i32 {
+    let mut sum = 0;
+    for e in v {
+        sum += e
+    }
+    sum
+}
+// **Exercise 01.2**: Write a function `vec_print` that takes a vector and prints all its elements.
+fn vec_print(v: Vec<i32>) {
+    for e in v {
+        println!("{}", e);
+    }
+}
+
 // With our refactored functions and methods, `main` now looks as follows:
 fn read_vec() -> Vec<i32> {
-    vec![18,5,7,2,9,27]
+    vec![18, 5, 7, 2, 9, 27]
 }
 pub fn main() {
     let vec = read_vec();
     let min = vec_min(vec);
-    unimplemented!()
+    min.print();
+    // println!("{}", sqr(2));
+    // println!("{}", vec_sum(vec));
+    // vec_print(vec);
 }
 // You will have to replace `part00` by `part01` in the `main` function in
 // `main.rs` to run this code.
-
-// **Exercise 01.1**: Write a function `vec_sum` that computes the sum of all values of a `Vec<i32>`.
-
-// **Exercise 01.2**: Write a function `vec_print` that takes a vector and prints all its elements.
-
